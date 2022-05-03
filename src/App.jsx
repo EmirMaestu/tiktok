cae6a4405da3c15dfabfe29ea4214d73f2f25c70
Emir Maestu <emir13_@hotmail.com>
2022-05-03 02:28:31 -0300

    feat(Root): pause and play video button

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import VideoPlayer from './components/VideoPlayer'
 
 function App() {
     const [count, setCount] = useState(0)
 
-    return <div className='App'>
-      <main>
-        <h1>tik tok clon</h1>
-      </main>
-    </div>
+    return (
+        <div className='App'>
+            <main>
+                <VideoPlayer />
+            </main>
+        </div>
+    )
 }
 
 export default App
