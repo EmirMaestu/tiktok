f4daa85b9de82eb0ba1814ea7eeb92bb20eaa1e2
Emir Maestu <emir13_@hotmail.com>
2022-05-04 00:41:03 -0300

    fix(Root):  add comments and share components

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,18 +1,27 @@
-import { Heart } from "../Icons/Heart"
+import { Heart } from '../Icons/Heart'
+import { Comment } from '../Icons/Comment'
+import { Share } from '../Icons/Share'
 import styles from './styles.module.css'
 
-
-export default function VideoPlayerActions(){
-    return(
+export default function VideoPlayerActions({
+    like = 1571,
+    comments = 700,
+    shares = 900,
+    hearted = false,
+}) {
+    return (
         <aside className={styles.actions}>
             <div className={styles.action}>
                 <Heart />
+                <strong title='like'>{like}</strong>
             </div>
             <div className={styles.action}>
-                <Heart />
+                <Comment />
+                <strong title='comments'>{comments}</strong>
             </div>
             <div className={styles.action}>
-                <Heart />
+                <Share />
+                <strong title='shares'>{shares}</strong>
             </div>
         </aside>
     )
